8d4027b4286e617b724127ccfdd5313eaadeaa75
ariana0505 <penaariana075@gmail.com>
2025-08-19 22:03:45 -0500

    dia 1- entrada, salida, y metodo

--- Dia 1/entrada_y_salida.py
+++ Dia 1/entrada_y_salida.py
@@ -2,3 +2,9 @@ import sys
 sys.stdout.write("Danos tu opinión: \n")#si no ponder \n primero te pedira el valor sin haber mostrado el mensaje 
 opinion = sys.stdin.readline().strip() #entrada para tu opinion
 sys.stdout.write(f"Tu opinión fue: {opinion}\n") #utiliza los f-string
+# puede ser util 
+for line in sys.stdin: # sys.stdin implicitamente te habre como un input
+    x = line.strip() #esto funcionara como un bucle infinito hasta que le des una opcion de terminar
+    if x == "break":# o cerrar directamente EOF
+        break
+    print("Dijiste:", x)
